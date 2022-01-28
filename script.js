1b13445603462db933e10b7abff286e05a7a1ae0
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-01-28 18:11:30 -0300

    seleção de pratos, bebidas e sobremesas funcional

---
+++ script.js
@@ -0,0 +1,72 @@
+function selecFrango(){
+    let borda = document.querySelector(".frango");
+    let bordaOut = document.querySelector(".carne");
+    let bordaOut2 = document.querySelector(".peixe");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecCarne(){
+    let borda = document.querySelector(".carne");
+    let bordaOut = document.querySelector(".peixe");
+    let bordaOut2 = document.querySelector(".frango");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecPeixe(){
+    let borda = document.querySelector(".peixe");
+    let bordaOut = document.querySelector(".carne");
+    let bordaOut2 = document.querySelector(".frango");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecCoca(){
+    let borda = document.querySelector(".coca");
+    let bordaOut = document.querySelector(".guarana");
+    let bordaOut2 = document.querySelector(".pepsi");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecGuarana(){
+    let borda = document.querySelector(".guarana");
+    let bordaOut = document.querySelector(".coca");
+    let bordaOut2 = document.querySelector(".pepsi");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecPepsi(){
+    let borda = document.querySelector(".pepsi");
+    let bordaOut = document.querySelector(".guarana");
+    let bordaOut2 = document.querySelector(".coca");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecPudim(){
+    let borda = document.querySelector(".pudim");
+    let bordaOut = document.querySelector(".brigadeiro");
+    let bordaOut2 = document.querySelector(".sunday");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecBrigadeiro(){
+    let borda = document.querySelector(".brigadeiro");
+    let bordaOut = document.querySelector(".pudim");
+    let bordaOut2 = document.querySelector(".sunday");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
+function selecSunday(){
+    let borda = document.querySelector(".sunday");
+    let bordaOut = document.querySelector(".brigadeiro");
+    let bordaOut2 = document.querySelector(".pudim");
+    borda.classList.add("selecionado");
+    bordaOut.classList.remove("selecionado");
+    bordaOut2.classList.remove("selecionado");
+}
